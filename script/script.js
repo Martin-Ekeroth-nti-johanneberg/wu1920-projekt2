7013f83f872880da637f55e6bb2c5405b392ca32
Martin-Ekeroth-nti-johanneberg <martin.ekeroth@elev.ga.ntig.se>
2020-05-20 10:59:01 +0200

    Sista förändringarna
    All info har jag skrivit i vårt delade dokument

--- script/script.js
+++ script/script.js
@@ -17,3 +17,27 @@ function slideshow(n) {
 
 }
 
+function changetext (textvar) {
+  rptstr = textvar.repeat(5)
+  if (textvar == "faq") {
+    text = "Detta är faq tabben " + textvar.repeat(10);
+  } if (textvar ==  'mylife') {
+    text = "Detta är My life tabben " + textvar.repeat(10);
+  } if (textvar ==  'about') {
+    text = "Detta är About tabben " + textvar.repeat(10);
+  } if (textvar ==  'misc') {
+    text = "Detta är Misc tabben " + textvar.repeat(10);
+  }
+  document.getElementById("abouttext").innerHTML = text;
+}
+
+function shopscript (styler) {
+  var all = document.getElementsByClassName('shopobject');
+  var objects = document.getElementsByClassName(styler);
+  for (n = 0; n < all.length; n++) {
+    all[n].style.display = 'none';
+  }
+  for (n = 0; n < objects.length; n++) {
+    objects[n].style.display = 'inline-block';
+  }
+}
